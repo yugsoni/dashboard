bcea8e30a04c7bec1c5beadd063714b78e4a4c59
Yug Developer <yug.spider@gmail.com>
2020-03-26 15:51:08 +0530

    make it better

--- resources/views/panels/navbar.blade.php
+++ resources/views/panels/navbar.blade.php
@@ -103,7 +103,7 @@
                         </ul>
                     </li>
                     <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                            <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">{{ Auth::user()->name }}</span><span class="user-status">Available</span></div><span><img class="round" src="{{asset('images/portrait/small/avatar-s-11.jpg') }}" alt="avatar" height="40" width="40" /></span>
+                            <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">yug</span><span class="user-status">Available</span></div><span><img class="round" src="{{asset('images/portrait/small/avatar-s-11.jpg') }}" alt="avatar" height="40" width="40" /></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="page-user-profile"><i class="feather icon-user"></i> Edit Profile</a><a class="dropdown-item" href="app-email"><i class="feather icon-mail"></i> My Inbox</a><a class="dropdown-item" href="app-todo"><i class="feather icon-check-square"></i> Task</a><a class="dropdown-item" href="app-chat"><i class="feather icon-message-square"></i> Chats</a>
                             <div class="dropdown-divider"></div><a class="dropdown-item" href="{{ route('register') }}"><i class="feather icon-power"></i> Logout</a>
